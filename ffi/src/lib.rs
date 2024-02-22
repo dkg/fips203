a6028f2c095050008b82467d73e3f57ada95034b
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2024-02-22 11:01:30 -0500

    do proper error handling in the library

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -34,14 +34,16 @@ pub extern "C" fn ml_kem_512_keygen(encaps_out: Option<&mut ml_kem_512_encaps_ke
                                     decaps_out: Option<&mut ml_kem_512_decaps_key>) -> u8 {
     use fips203::traits::{KeyGen, SerDes};
 
-    if let (Some(encaps_out), Some(decaps_out)) = (encaps_out, decaps_out) {
-        let (ek, dk) = fips203::ml_kem_512::KG::try_keygen_vt().unwrap();
-        encaps_out.data = ek.into_bytes();
-        decaps_out.data = dk.into_bytes();
-        return ML_KEM_OK;
-    } else {
+    let (Some(encaps_out), Some(decaps_out)) = (encaps_out, decaps_out) else  {
         return ML_KEM_NULL_PTR_ERROR;
-    }
+    };
+    let Ok((ek, dk)) = fips203::ml_kem_512::KG::try_keygen_vt() else {
+        return ML_KEM_KEYGEN_ERROR;
+    };
+
+    encaps_out.data = ek.into_bytes();
+    decaps_out.data = dk.into_bytes();
+    return ML_KEM_OK;
 }
 
 #[no_mangle]
@@ -50,16 +52,19 @@ pub extern "C" fn ml_kem_512_encaps(encaps: Option<&ml_kem_512_encaps_key>,
                                     shared_secret_out: Option<&mut ml_kem_shared_secret>) -> u8 {
     use fips203::traits::{Encaps, SerDes};
 
-    if let (Some(encaps), Some(ciphertext_out), Some(shared_secret_out)) = (encaps, ciphertext_out, shared_secret_out) {
-        let ek = fips203::ml_kem_512::EncapsKey::try_from_bytes(encaps.data).unwrap();
-        let (ssk, ct) = ek.try_encaps_vt().unwrap();
-        shared_secret_out.data = ssk.into_bytes();
-        ciphertext_out.data = ct.into_bytes();
-
-        return ML_KEM_OK;
-    } else {
+    let (Some(encaps), Some(ciphertext_out), Some(shared_secret_out)) = (encaps, ciphertext_out, shared_secret_out) else {
         return ML_KEM_NULL_PTR_ERROR;
-    }
+    };
+    let Ok(ek) = fips203::ml_kem_512::EncapsKey::try_from_bytes(encaps.data) else {
+        return ML_KEM_DESERIALIZATION_ERROR;
+    };
+    let Ok((ssk, ct)) = ek.try_encaps_vt() else {
+        return ML_KEM_ENCAPS_ERROR;
+    };
+
+    shared_secret_out.data = ssk.into_bytes();
+    ciphertext_out.data = ct.into_bytes();
+    return ML_KEM_OK;
 }
 
 #[no_mangle]
@@ -68,17 +73,21 @@ pub extern "C" fn ml_kem_512_decaps(decaps: Option<&ml_kem_512_decaps_key>,
                                     shared_secret_out: Option<&mut ml_kem_shared_secret>) -> u8 {
     use fips203::traits::{Decaps, SerDes};
 
-    if let (Some(decaps), Some(ciphertext), Some(shared_secret_out)) = (decaps, ciphertext, shared_secret_out) {
-        let dk = fips203::ml_kem_512::DecapsKey::try_from_bytes(decaps.data).unwrap();
-        let ct = fips203::ml_kem_512::CipherText::try_from_bytes(ciphertext.data).unwrap();
-        let ssk = dk.try_decaps_vt(&ct).unwrap();
-
-        shared_secret_out.data = ssk.into_bytes();
-
-        return ML_KEM_OK;
-    } else {
+    let (Some(decaps), Some(ciphertext), Some(shared_secret_out)) = (decaps, ciphertext, shared_secret_out) else {
         return ML_KEM_NULL_PTR_ERROR;
-    }
+    };
+    let Ok(dk) = fips203::ml_kem_512::DecapsKey::try_from_bytes(decaps.data) else {
+        return ML_KEM_DESERIALIZATION_ERROR;
+    };
+    let Ok(ct) = fips203::ml_kem_512::CipherText::try_from_bytes(ciphertext.data) else {
+        return ML_KEM_DESERIALIZATION_ERROR;
+    };       
+    let Ok(ssk) = dk.try_decaps_vt(&ct) else {
+        return ML_KEM_DECAPS_ERROR;
+    };
+
+    shared_secret_out.data = ssk.into_bytes();
+    return ML_KEM_OK;
 }
 
 
@@ -103,15 +112,16 @@ pub extern "C" fn ml_kem_768_keygen(encaps_out: Option<&mut ml_kem_768_encaps_ke
                                     decaps_out: Option<&mut ml_kem_768_decaps_key>) -> u8 {
     use fips203::traits::{KeyGen, SerDes};
 
-    if let (Some(encaps_out), Some(decaps_out)) = (encaps_out, decaps_out) {
-        let (ek, dk) = fips203::ml_kem_768::KG::try_keygen_vt().unwrap();
-        encaps_out.data = ek.into_bytes();
-        decaps_out.data = dk.into_bytes();
-
-        return ML_KEM_OK;
-    } else {
+    let (Some(encaps_out), Some(decaps_out)) = (encaps_out, decaps_out) else {
         return ML_KEM_NULL_PTR_ERROR;
-    }
+    };
+    let Ok((ek, dk)) = fips203::ml_kem_768::KG::try_keygen_vt() else {
+        return ML_KEM_KEYGEN_ERROR;
+    };
+
+    encaps_out.data = ek.into_bytes();
+    decaps_out.data = dk.into_bytes();
+    return ML_KEM_OK;
 }
 
 #[no_mangle]
@@ -120,16 +130,19 @@ pub extern "C" fn ml_kem_768_encaps(encaps: Option<&ml_kem_768_encaps_key>,
                                     shared_secret_out: Option<&mut ml_kem_shared_secret>) -> u8 {
     use fips203::traits::{Encaps, SerDes};
 
-    if let (Some(encaps), Some(ciphertext_out), Some(shared_secret_out)) = (encaps, ciphertext_out, shared_secret_out) {
-        let ek = fips203::ml_kem_768::EncapsKey::try_from_bytes(encaps.data).unwrap();
-        let (ssk, ct) = ek.try_encaps_vt().unwrap();
-        shared_secret_out.data = ssk.into_bytes();
-        ciphertext_out.data = ct.into_bytes();
-
-        return ML_KEM_OK;
-    } else {
+    let (Some(encaps), Some(ciphertext_out), Some(shared_secret_out)) = (encaps, ciphertext_out, shared_secret_out) else {
         return ML_KEM_NULL_PTR_ERROR;
-    }
+    };
+    let Ok(ek) = fips203::ml_kem_768::EncapsKey::try_from_bytes(encaps.data) else {
+        return ML_KEM_DESERIALIZATION_ERROR;
+    };
+    let Ok((ssk, ct)) = ek.try_encaps_vt() else {
+        return ML_KEM_ENCAPS_ERROR;
+    };
+
+    shared_secret_out.data = ssk.into_bytes();
+    ciphertext_out.data = ct.into_bytes();
+    return ML_KEM_OK;
 }
 
 #[no_mangle]
@@ -138,17 +151,21 @@ pub extern "C" fn ml_kem_768_decaps(decaps: Option<&ml_kem_768_decaps_key>,
                                     shared_secret_out: Option<&mut ml_kem_shared_secret>) -> u8 {
     use fips203::traits::{Decaps, SerDes};
 
-    if let (Some(decaps), Some(ciphertext), Some(shared_secret_out)) = (decaps, ciphertext, shared_secret_out) {
-        let dk = fips203::ml_kem_768::DecapsKey::try_from_bytes(decaps.data).unwrap();
-        let ct = fips203::ml_kem_768::CipherText::try_from_bytes(ciphertext.data).unwrap();
-        let ssk = dk.try_decaps_vt(&ct).unwrap();
-
-        shared_secret_out.data = ssk.into_bytes();
-
-        return ML_KEM_OK;
-    } else {
+    let (Some(decaps), Some(ciphertext), Some(shared_secret_out)) = (decaps, ciphertext, shared_secret_out) else {
         return ML_KEM_NULL_PTR_ERROR;
-    }
+    };
+    let Ok(dk) = fips203::ml_kem_768::DecapsKey::try_from_bytes(decaps.data) else {
+        return ML_KEM_DESERIALIZATION_ERROR;
+    };
+    let Ok(ct) = fips203::ml_kem_768::CipherText::try_from_bytes(ciphertext.data) else {
+        return ML_KEM_DESERIALIZATION_ERROR;
+    };
+    let Ok(ssk) = dk.try_decaps_vt(&ct) else {
+        return ML_KEM_DECAPS_ERROR;
+    };
+
+    shared_secret_out.data = ssk.into_bytes();
+    return ML_KEM_OK;
 }
 
 
@@ -172,15 +189,16 @@ pub extern "C" fn ml_kem_1024_keygen(encaps_out: Option<&mut ml_kem_1024_encaps_
                                      decaps_out: Option<&mut ml_kem_1024_decaps_key>) -> u8 {
     use fips203::traits::{KeyGen, SerDes};
 
-    if let (Some(encaps_out), Some(decaps_out)) = (encaps_out, decaps_out) {
-        let (ek, dk) = fips203::ml_kem_1024::KG::try_keygen_vt().unwrap();
-        encaps_out.data = ek.into_bytes();
-        decaps_out.data = dk.into_bytes();
-
-        return ML_KEM_OK;
-    } else {
+    let (Some(encaps_out), Some(decaps_out)) = (encaps_out, decaps_out) else {
         return ML_KEM_NULL_PTR_ERROR;
-    }
+    };
+    let Ok((ek, dk)) = fips203::ml_kem_1024::KG::try_keygen_vt() else {
+        return ML_KEM_KEYGEN_ERROR;
+    };
+
+    encaps_out.data = ek.into_bytes();
+    decaps_out.data = dk.into_bytes();
+    return ML_KEM_OK;
 }
 
 #[no_mangle]
@@ -189,16 +207,19 @@ pub extern "C" fn ml_kem_1024_encaps(encaps: Option<&ml_kem_1024_encaps_key>,
                                      shared_secret_out: Option<&mut ml_kem_shared_secret>) -> u8 {
     use fips203::traits::{Encaps, SerDes};
 
-    if let (Some(encaps), Some(ciphertext_out), Some(shared_secret_out)) = (encaps, ciphertext_out, shared_secret_out) {
-        let ek = fips203::ml_kem_1024::EncapsKey::try_from_bytes(encaps.data).unwrap();
-        let (ssk, ct) = ek.try_encaps_vt().unwrap();
-        shared_secret_out.data = ssk.into_bytes();
-        ciphertext_out.data = ct.into_bytes();
-
-        return ML_KEM_OK;
-    } else {
+    let (Some(encaps), Some(ciphertext_out), Some(shared_secret_out)) = (encaps, ciphertext_out, shared_secret_out) else {
         return ML_KEM_NULL_PTR_ERROR;
-    }
+    };
+    let Ok(ek) = fips203::ml_kem_1024::EncapsKey::try_from_bytes(encaps.data) else {
+        return ML_KEM_DESERIALIZATION_ERROR;
+    };
+    let Ok((ssk, ct)) = ek.try_encaps_vt() else {
+        return ML_KEM_ENCAPS_ERROR;
+    };
+
+    shared_secret_out.data = ssk.into_bytes();
+    ciphertext_out.data = ct.into_bytes();
+    return ML_KEM_OK;
 }
 
 #[no_mangle]
@@ -207,15 +228,19 @@ pub extern "C" fn ml_kem_1024_decaps(decaps: Option<&ml_kem_1024_decaps_key>,
                                      shared_secret_out: Option<&mut ml_kem_shared_secret>) -> u8 {
     use fips203::traits::{Decaps, SerDes};
 
-    if let (Some(decaps), Some(ciphertext), Some(shared_secret_out)) = (decaps, ciphertext, shared_secret_out) {
-        let dk = fips203::ml_kem_1024::DecapsKey::try_from_bytes(decaps.data).unwrap();
-        let ct = fips203::ml_kem_1024::CipherText::try_from_bytes(ciphertext.data).unwrap();
-        let ssk = dk.try_decaps_vt(&ct).unwrap();
-
-        shared_secret_out.data = ssk.into_bytes();
-
-        return ML_KEM_OK;
-    } else {
+    let (Some(decaps), Some(ciphertext), Some(shared_secret_out)) = (decaps, ciphertext, shared_secret_out) else {
         return ML_KEM_NULL_PTR_ERROR;
-    }
+    };
+    let Ok(dk) = fips203::ml_kem_1024::DecapsKey::try_from_bytes(decaps.data) else {
+        return ML_KEM_DESERIALIZATION_ERROR;
+    };
+    let Ok(ct) = fips203::ml_kem_1024::CipherText::try_from_bytes(ciphertext.data) else {
+        return ML_KEM_DESERIALIZATION_ERROR;
+    };
+    let Ok(ssk) = dk.try_decaps_vt(&ct) else {
+        return ML_KEM_DECAPS_ERROR;
+    };
+
+    shared_secret_out.data = ssk.into_bytes();
+    return ML_KEM_OK;
 }
